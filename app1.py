822a2399f3dff9ac76fcfd38f1867ab0882768c2
Gaspa <80415632+s20143@users.noreply.github.com>
2022-04-09 12:51:59 +0200

    Update app1.py

--- app1.py
+++ app1.py
@@ -18,34 +18,34 @@ embarked_d = {0:"Cherbourg", 1:"Queenstown", 2:"Southampton"}
 
 def main():
 
-	st.set_page_config(page_title="Titanic")
-	overview = st.container()
-	left, right = st.columns(2)
-	prediction = st.container()
+    st.set_page_config(page_title="Titanic")
+    overview = st.container()
+    left, right = st.columns(2)
+    prediction = st.container()
 
-	st.image("https://pl.wikipedia.org/wiki/RMS_Titanic#/media/Plik:RMS_Titanic_3.jpg")
+    st.image("https://pl.wikipedia.org/wiki/RMS_Titanic#/media/Plik:RMS_Titanic_3.jpg")
 
-	with overview:
-		st.title("Titanic")
+    with overview:
+        st.title("Titanic")
 
-	with left:
-		sex_radio = st.radio( "Płeć", list(sex_d.keys()), format_func=lambda x : sex_d[x] )
-		embarked_radio = st.radio( "Port zaokrętowania", list(embarked_d.keys()), index=2, format_func= lambda x: embarked_d[x] )
-        	pclass_radio = st.radio( "Klasa", list(pclass_d.keys()), format_func=lambda x : pclass_d[x] )
-	with right:
-		age_slider = st.slider("Wiek", value=1, min_value=0.42, max_value=80)
-		sibsp_slider = st.slider("Liczba rodzeństwa i/lub partnera", min_value=0, max_value=8)
-		parch_slider = st.slider("Liczba rodziców i/lub dzieci", min_value=0, max_value=6)
-		fare_slider = st.slider("Cena biletu", min_value=0, max_value=512.3292, step=1)
+    with left:
+        sex_radio = st.radio( "Płeć", list(sex_d.keys()), format_func=lambda x : sex_d[x] )
+        embarked_radio = st.radio( "Port zaokrętowania", list(embarked_d.keys()), index=2, format_func= lambda x: embarked_d[x] )
+        pclass_radio = st.radio( "Klasa", list(pclass_d.keys()), format_func=lambda x : pclass_d[x] )
+    with right:
+        age_slider = st.slider("Wiek", value=1, min_value=0.42, max_value=80)
+        sibsp_slider = st.slider("Liczba rodzeństwa i/lub partnera", min_value=0, max_value=8)
+        parch_slider = st.slider("Liczba rodziców i/lub dzieci", min_value=0, max_value=6)
+        fare_slider = st.slider("Cena biletu", min_value=0, max_value=512.3292, step=1)
 
-	data = [[pclass_radio, sex_radio,  age_slider, sibsp_slider, parch_slider, fare_slider, embarked_radio]]
-	survival = model.predict(data)
-	s_confidence = model.predict_proba(data)
+    data = [[pclass_radio, sex_radio,  age_slider, sibsp_slider, parch_slider, fare_slider, embarked_radio]]
+    survival = model.predict(data)
+    s_confidence = model.predict_proba(data)
 
-	with prediction:
-		st.subheader("Czy taka osoba przeżyłaby katastrofę?")
-		st.subheader(("Tak" if survival[0] == 1 else "Nie"))
-		st.write("Pewność predykcji {0:.2f} %".format(s_confidence[0][survival][0] * 100))
+    with prediction:
+        st.subheader("Czy taka osoba przeżyłaby katastrofę?")
+        st.subheader(("Tak" if survival[0] == 1 else "Nie"))
+        st.write("Pewność predykcji {0:.2f} %".format(s_confidence[0][survival][0] * 100))
 
 if __name__ == "__main__":
     main()
